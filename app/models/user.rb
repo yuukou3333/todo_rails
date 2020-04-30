927952b770693b22d92d2ce8dcafd5a2152755f5
千田裕耕 <sendahiroshikou@YuukounoMacBook-Air.local>
2020-04-30 10:47:46 +0900

    nameカラムの追加（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presense: true, length: {maximum: 20}
+  validates :name, presence: true, length: {maximum: 20}
 end
